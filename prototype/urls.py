9e5673c9f32a7f84da9d375eced54f6ac1373eb6
Ri <Ri@Profs-MacBook-Air.local>
2018-04-17 17:38:54 -0400

    home, 2 logins, user video

--- prototype/urls.py
+++ prototype/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "prototype"
 
 urlpatterns = [
-    url(r'^$', views.about, name="about"),
+    url(r'^$', views.home, name="home"),
     url(r'about/$', views.about, name="about"),
     url(r'people/$', views.people, name="people"),
     url(r'contact/$', views.contact, name="contact"),
@@ -14,7 +14,9 @@ urlpatterns = [
     url(r'signup/$', views.signup, name="signup"),
     url(r'login/$', views.login, name="login"),
     url(r'in/$', views.in_land, name="in"),
+    url(r'in_us/$', views.in_land_us, name="in_us"),
     url(r'practicevideos/$', views.prac, name="prac"),
+    url(r'practicevideos_us/$', views.prac_user, name="prac_user"),
 
 
 
--- prototype/views.py
+++ prototype/views.py
@@ -7,6 +7,9 @@ def landing_page(request):
 def about(request):
     return render(request, "about.html")
 
+def home(request):
+    return render(request, "vt_home.html")
+
 def people(request):
     return render(request, "people.html")
 
@@ -28,7 +31,12 @@ def login(request):
 def in_land(request):
     return render(request, "in_landing.html")
 
+def in_land_us(request):
+    return render(request, "in_landing_us.html")
+
 
 def prac(request):
     return render(request, "practicevideos.html")
 
+def prac_user(request):
+    return render(request, "practicevideos_danceteam.html")
